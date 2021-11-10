3d58d3c9ba31fb3b880cc1d0a3697761cb858bd4
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-10 14:59:05 +0100

    init

---
+++ src/hu/petrik/halmazoop/Main.java
@@ -0,0 +1,8 @@
+package hu.petrik.halmazoop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/hu/petrik/halmazoop/Set/Set.java
@@ -0,0 +1,4 @@
+package hu.petrik.halmazoop.Set;
+
+public class Set {
+}
